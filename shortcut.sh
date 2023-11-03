84140b8f850d40b9ee975913fa15f15bd88caa0d
pazerW <xiedizi@gmail.com>
2023-11-03 13:12:10 +0800

    update 1.0.1 1. play music 2.focus mode

---
+++ shortcut.sh
@@ -0,0 +1,2 @@
+do shell script "open 'shortcuts://run-shortcut?name=PlayMusic'"
+do shell script "open 'shortcuts://run-shortcut?name=Clock&input=1500'"
---
+++ start.scpt
@@ -0,0 +1,5 @@
+on run argv
+	tell application "Tyme"
+		StartTrackerForTaskID argv
+	end tell
+end run
